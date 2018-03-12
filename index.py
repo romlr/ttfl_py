adc4dc83539846ff0a690678ee7f5e8e4d011890
Romain HEROUARD <romain.herouard@toulouse.viveris.com>
2018-03-12 15:31:58 +0100

    add plotting

--- index.py
+++ index.py
@@ -28,14 +28,14 @@ def main():
         pid = player.get_player(first_name, last_name)
 
         info = player.PlayerSummary(pid).info()
-        names.append("%s %s" % (info['FIRST_NAME'].values, info['LAST_NAME'].values))
+        names.append("%s %s" % (info['FIRST_NAME'].values[0], info['LAST_NAME'].values[0]))
 
         last_splits =  player.PlayerGeneralSplits(pid, season="2017-18", measure_type="Base", last_n_games=last_n_games).overall()
-        last_n_games_fp.append(last_splits['NBA_FANTASY_PTS'].values)
+        last_n_games_fp.append(last_splits['NBA_FANTASY_PTS'].values[0])
 
         overall_splits =  player.PlayerGeneralSplits(pid, season="2017-18", measure_type="Base").overall()
-        overall_fp.append(overall_splits['NBA_FANTASY_PTS'].values)
-        overall_gp.append(overall_splits['GP'].values)
+        overall_fp.append(overall_splits['NBA_FANTASY_PTS'].values[0])
+        overall_gp.append(overall_splits['GP'].values[0])
 
 
     print "FANTASY POINTS over %d last games / season overall (games played)" % last_n_games
@@ -44,12 +44,12 @@ def main():
     for idx in range(len(deck)):
         print "%s:  %.1f / %.1f (%d)" % (names[idx], last_n_games_fp[idx], overall_fp[idx], overall_gp[idx])
 
-    # data = [pgo.Bar(
-    #     x = names,
-    #     y = last_n_games_fp
-    # )]
-    #
-    # po.plot(data)
+    data = [pgo.Bar(
+        x = names,
+        y = last_n_games_fp
+    )]
+
+    po.plot(data)
 
 
 if __name__ == "__main__":
