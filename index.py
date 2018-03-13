140b2ba9f9c95e878b31181ad51d8ff1ee84272d
Romain HEROUARD <romain.herouard@toulouse.viveris.com>
2018-03-13 16:59:55 +0100

    add comments

--- index.py
+++ index.py
@@ -11,20 +11,29 @@ def get_player_trend(pid, nb_games, name):
     game_n_fp = []
     game_n_ttfl = []
 
+    # ---
+
+    # fetch players splits (totals) for each game
     for idx in range(nb_games, 0, -1):
         splits = player.PlayerGeneralSplits(pid, season="2017-18", measure_type="Base",
                                              last_n_games=idx, per_mode='Totals').overall()
 
+        # get nba fantasy points and calculate ttfl score
         total_fp.append(splits['NBA_FANTASY_PTS'].values[0])
         total_ttfl.append(get_ttfl_score(splits))
 
+    # calculate individual game splits for each game based on totals fetched
     for idx in range(0, nb_games - 1):
         game_n_fp.append(total_fp[idx] - total_fp[idx + 1])
         game_n_ttfl.append(total_ttfl[idx] - total_ttfl[idx + 1])
 
+    # add latests game splits
     game_n_fp.append(total_fp[-1])
     game_n_ttfl.append(total_ttfl[-1])
 
+    # ---
+
+    # trace plots for nba fp and ttfl score trends
     trace1 = pgo.Scatter( x=range(0, nb_games),
                          y=game_n_fp,
                          mode='lines+markers',
@@ -42,11 +51,14 @@ def get_player_trend(pid, nb_games, name):
 
 def get_ttfl_score(splits):
 
+    # tables containing stats categories used for ttfl score calculation
     bonus = ['PTS', 'REB', 'AST', 'STL', 'BLK', 'FGM', 'FG3M', 'FTM']
     malus = ['TOV', 'FGA', 'FG3A', 'FTA']
 
+    # init ttfl score to zero
     ttfl_score = 0
 
+    # calculate bonus part (PTS + REB + AST + STL + BLK + 2*FGM + 2*FGM3 + 2*FTM)
     for cat in bonus:
         n = 1
 
@@ -55,16 +67,15 @@ def get_ttfl_score(splits):
 
         ttfl_score = ttfl_score + n*splits[cat].values[0]
 
+    # remove malus part from score (- TOV - FGA - FG3A - FTA)
     for cat in malus:
         ttfl_score = ttfl_score - splits[cat].values[0]
 
+    # return calculated score
     return ttfl_score
 
-def get_deck_fp(deck):
 
-    nb_games = 10
-
-    # -----------------------------------------------------------------------------------------------
+def get_deck_ratings(deck, nb_games):
 
     pids = []
     names = []
@@ -80,25 +91,40 @@ def get_deck_fp(deck):
 
     # ---
 
+    # iterate through deck and fetch first and last name
     for first_name, last_name in deck:
+
+        # fetch player id for each player based on first and last name
         pid  = player.get_player(first_name, last_name, season="2017-18")
         pids.append(pid)
 
+        # fetch player names from info summary - TODO: add player summary, photo, etc.
         info = player.PlayerSummary(pid).info()
         name = '%s %s' % (info['FIRST_NAME'].values[0], info['LAST_NAME'].values[0])
         names.append(name)
 
+        # fetch player splits for last N games (passed as parameter)
         last_splits =  player.PlayerGeneralSplits(pid, season="2017-18", measure_type="Base", last_n_games=nb_games).overall()
+
+        # fetch nba fp from splits
         last_n_games_fp.append(last_splits['NBA_FANTASY_PTS'].values[0])
 
+        # calculate ttfl score from splits
         last_n_games_ttfl_score.append(get_ttfl_score(last_splits))
 
+        # fetch player splits for overall season
         overall_splits =  player.PlayerGeneralSplits(pid, season="2017-18", measure_type="Base").overall()
+
+        # fetch nba fp and games played from splits
         overall_fp.append(overall_splits['NBA_FANTASY_PTS'].values[0])
         overall_gp.append(overall_splits['GP'].values[0])
 
+        # calculate ttfl score from splits
         overall_ttfl_score.append(get_ttfl_score(overall_splits))
 
+    # ---
+
+    # trace bars representing last n games and overall season nba fp and ttfl ratings
     trace1 = pgo.Bar(x=names, y=last_n_games_fp, name='last %d games fp avg' % nb_games)
 
     trace2 = pgo.Bar(x=names, y=overall_fp, name='season overall fp avg')
@@ -118,15 +144,18 @@ def get_deck_fp(deck):
     for pid, name in zip(pids, names):
         get_player_trend(pid, nb_games, name)
 
+
 if __name__ == "__main__":
     # execute only if run as a script
 
-    deck = [
+    DECK = [
         ["Anthony", "Davis"],
-        # ["Russell", "Westbrook"],
-        # ["James", "Harden"],
-        # ["Kevin", "Durant"],
-        # ["Giannis", "Antetokounmpo"],
+        ["Russell", "Westbrook"],
+        ["James", "Harden"],
+        ["Kevin", "Durant"],
+        ["Giannis", "Antetokounmpo"]
     ]
 
-    get_deck_fp(deck)
+    NB_GAMES = 5
+
+    get_deck_ratings(DECK, NB_GAMES)
