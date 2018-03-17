8b6e8eeddf3add7dd18b95b88e5b90cc6e35de8e
romlr <romain.herouard@gmail.com>
2018-03-17 17:03:47 +0100

    modify shortlist print

--- __init__.py
+++ __init__.py
@@ -7,16 +7,19 @@ if __name__ == "__main__":
     # execute only if run as a script
 
     DATE = datetime.date(2018, 03, 17)
-    NB_PLAYERS = 3
-    NB_SHORTLIST = 10
+    NB_PLAYERS = 5
+    NB_SHORTLIST = 15
 
     shortlist = games.get_day_games(DATE, NB_PLAYERS, NB_SHORTLIST)
 
     names = shortlist['PLAYER_NAME'].values
     ttfl_score = shortlist['TTFL_SCORE'].values
+    nba_fp = shortlist['NBA_FANTASY_PTS']
 
+    print "SHORTLIST"
+    print
     for i in range(0,NB_SHORTLIST):
-        print "%s - TTFL:%.1f" % (names[i], ttfl_score[i])
+        print "%s (TTFL:%.1f, NBA FP:%.1f)" % (names[i], ttfl_score[i], nba_fp[i])
 
     my_deck = shortlist['PLAYER_ID'].values
 
