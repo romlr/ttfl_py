06bae2c9301b078d3e5772654a683668933cec92
romlr <romain.herouard@gmail.com>
2018-03-18 17:01:05 +0100

    minor correction

--- __init__.py
+++ __init__.py
@@ -6,15 +6,15 @@ import deck, games, team
 if __name__ == "__main__":
     # execute only if run as a script
 
-    DATE = datetime.date(2018, 03, 17)
+    TODAY = datetime.date.today()
     NB_PLAYERS = 5
     NB_SHORTLIST = 15
 
-    shortlist = games.get_day_games(DATE, NB_PLAYERS, NB_SHORTLIST)
+    shortlist = games.get_day_games(TODAY, NB_PLAYERS, NB_SHORTLIST)
 
     names = shortlist['PLAYER_NAME'].values
     ttfl_score = shortlist['TTFL_SCORE'].values
-    nba_fp = shortlist['NBA_FANTASY_PTS']
+    nba_fp = shortlist['NBA_FANTASY_PTS'].values
 
     print "SHORTLIST"
     print
